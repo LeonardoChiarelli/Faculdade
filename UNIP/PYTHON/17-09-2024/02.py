4c212e1f56c1d22746e20b2e2a004fdee60f7c7c
Leonardo Chiarelli <162373587+LeonardoChiarelli@users.noreply.github.com>
2024-09-17 19:37:32 -0300

    Create 02.py

---
+++ UNIP/PYTHON/17-09-2024/02.py
@@ -0,0 +1,8 @@
+idade = int(input("Informe sua idade: "))
+
+if idade < 18:
+    print("Você é menor de idade")
+elif idade >= 65:
+    print("Você é uma pessoa idosa")
+else:
+    print("Você é maior de idade")
