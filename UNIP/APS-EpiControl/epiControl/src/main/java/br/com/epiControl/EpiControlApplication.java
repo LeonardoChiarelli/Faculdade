ac31cd38fcfa5471fbaf2c4bec0fd01f2a8612fa
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-03-11 19:19:17 -0300

    Create Structure

---
+++ UNIP/APS-EpiControl/epiControl/src/main/java/br/com/epiControl/EpiControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.epiControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiControlApplication.class, args);
+	}
+
+}
---
+++ UNIP/APS-EpiControl/epiControl/src/test/java/br/com/epiControl/EpiControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.epiControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
